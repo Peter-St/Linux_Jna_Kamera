90f0e7207fd8f6d93e5fc2eb5d86c9c919bb1ec0
Peter-St <stoiber.peter@aon.at>
2019-02-26 13:22:56 +0100

    This class is uesed for converting the frame to jpg (for uncompressed format OpenCV is used

---
+++ src/humer/kamera/ConvertStream.java
@@ -0,0 +1,287 @@
+/*
+ * Copyright 2019 peter.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package humer.kamera;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import org.bytedeco.javacpp.BytePointer;
+import org.bytedeco.javacpp.IntPointer;
+import static org.bytedeco.javacpp.opencv_core.CV_8UC2;
+import static org.bytedeco.javacpp.opencv_core.CV_8UC3;
+import org.bytedeco.javacpp.opencv_core.Mat;
+import static org.bytedeco.javacpp.opencv_imgcodecs.imencode;
+import static org.bytedeco.javacpp.opencv_imgproc.COLOR_YUV2BGR_YUY2;
+import static org.bytedeco.javacpp.opencv_imgproc.cvtColor;
+/*import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgproc.Imgproc;
+*/
+
+
+
+/**
+ *
+ * @author peter
+ */
+public class ConvertStream {
+    
+    int imageWidth;
+    int imageHeight;
+    
+    public int exit = 0;
+    public boolean bildaufnahme = false;
+    Date date;
+    SimpleDateFormat dateFormat;
+    File file;
+    int a = 0;
+    BytePointer b= new BytePointer();
+    byte[] jpg;
+    ByteBuffer bb;
+    
+    
+    public ConvertStream(int Width,  int Height) {
+        this.imageWidth = Width;
+        this.imageHeight = Height;
+        //org.opencv.loadLibrary();
+        //System.load("/usr/lib/jni/libopencv_java320.so");
+        //System.load("/usr/lib/jni/libopencv_java320.so");
+        
+        //System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
+    
+    private void writeBytesToFile(String fileName, byte[] data) throws FileNotFoundException, IOException  {
+        FileOutputStream fileOutputStream = null;
+        try {
+            fileOutputStream = new FileOutputStream(fileName);
+            fileOutputStream.write(data);
+            fileOutputStream.flush();
+        } finally {
+            fileOutputStream.close();
+        }
+    }
+    
+    public byte[] processReceivedVideoFrame(byte[] frameData, int videoformat){
+        
+        switch (videoformat) {
+            case 0:
+                byte[] jpegFrameData = convertMjpegFrameToJpegKamera(frameData);
+
+                //   String fileName = new File(Environment.getExternalStorageDirectory(), "temp_usbcamtest1.jpg").getPath();
+                //    writeBytesToFile(fileName, jpegFrameData);
+
+
+                if (bildaufnahme) {
+                    bildaufnahme = false ;
+                    date = new Date() ;
+                    dateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss") ;
+                    String rootPath = "target/test.dump";
+
+
+                    file = new File(rootPath);
+                    if (!file.exists()) {
+                        file.mkdirs();
+                    }
+                    String fileName = new File(rootPath + dateFormat.format(date) + ".jpg").getPath() ;
+                    try {
+                        writeBytesToFile(fileName, jpegFrameData);
+                    } catch (IOException ex) {
+                        Logger.getLogger(ConvertStream.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                return jpegFrameData;
+                
+            case 1:
+                a ++;
+
+                Mat yuv = new Mat(imageHeight, imageWidth, CV_8UC2, new BytePointer(frameData));
+                Mat rgbMat = new Mat(imageHeight, imageWidth, CV_8UC3);
+                cvtColor(yuv, rgbMat, COLOR_YUV2BGR_YUY2);
+        
+                byte[] mRgbImage = new byte[rgbMat.channels() * rgbMat.cols() * rgbMat.rows()];
+                imencode(".jpg", rgbMat, mRgbImage, null); 
+                return mRgbImage;
+            default: return null;
+        }
+        
+    }
+    
+ 
+    
+    byte[] processReceivedVideoFrameYUY2(byte[] frameData) {
+        a ++;
+
+        Mat yuv = new Mat(imageHeight, imageWidth, CV_8UC2, new BytePointer(frameData));
+        Mat rgbMat = new Mat(imageHeight, imageWidth, CV_8UC3);
+        cvtColor(yuv, rgbMat, COLOR_YUV2BGR_YUY2);
+        
+        byte[] mRgbImage = new byte[rgbMat.channels() * rgbMat.cols() * rgbMat.rows()];
+        imencode(".jpg", rgbMat, mRgbImage, null); 
+        
+        return mRgbImage;
+    }
+    
+    public byte[] processReceivedMJpegVideoFrameKamera(byte[] mjpegFrameData) {
+
+        byte[] jpegFrameData = convertMjpegFrameToJpegKamera(mjpegFrameData);
+
+        //   String fileName = new File(Environment.getExternalStorageDirectory(), "temp_usbcamtest1.jpg").getPath();
+        //    writeBytesToFile(fileName, jpegFrameData);
+
+
+        if (bildaufnahme) {
+            bildaufnahme = false ;
+            date = new Date() ;
+            dateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss") ;
+            String rootPath = "target/test.dump";
+
+
+                file = new File(rootPath);
+                if (!file.exists()) {
+                    file.mkdirs();
+                }
+                String fileName = new File(rootPath + dateFormat.format(date) + ".jpg").getPath() ;
+            try {
+                writeBytesToFile(fileName, jpegFrameData);
+            } catch (IOException ex) {
+                Logger.getLogger(ConvertStream.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return jpegFrameData;
+    }
+    
+    // see USB video class standard, USB_Video_Payload_MJPEG_1.5.pdf
+    private byte[] convertMjpegFrameToJpegKamera(byte[] frameData)  {
+        int frameLen = frameData.length;
+        while (frameLen > 0 && frameData[frameLen - 1] == 0) {
+            frameLen--;
+        }
+        //  if (frameLen < 100 || (frameData[0] & 0xff) != 0xff || (frameData[1] & 0xff) != 0xD8 || (frameData[frameLen - 2] & 0xff) != 0xff || (frameData[frameLen - 1] & 0xff) != 0xd9) {
+        //        throw new Exception("Invalid MJPEG frame structure, length=" + frameData.length);
+        //  }
+        boolean hasHuffmanTable = findJpegSegment(frameData, frameLen, 0xC4) != -1;
+        exit = 0;
+        if (hasHuffmanTable) {
+            if (frameData.length == frameLen) {
+                return frameData;
+            }
+            return Arrays.copyOf(frameData, frameLen);
+        } else {
+            int segmentDaPos = findJpegSegment(frameData, frameLen, 0xDA);
+
+            try {if (segmentDaPos == -1) {
+                exit = 1;
+            }
+            } catch (Exception e) {
+                System.out.println("Segment 0xDA not found in MJPEG frame data.");}
+      //          throw new Exception("Segment 0xDA not found in MJPEG frame data.");
+            if (exit ==0) {
+                byte[] a = new byte[frameLen + mjpgHuffmanTable.length];
+                System.arraycopy(frameData, 0, a, 0, segmentDaPos);
+                System.arraycopy(mjpgHuffmanTable, 0, a, segmentDaPos, mjpgHuffmanTable.length);
+                System.arraycopy(frameData, segmentDaPos, a, segmentDaPos + mjpgHuffmanTable.length, frameLen - segmentDaPos);
+                return a;
+            } else
+                return null;
+
+
+        }
+    }
+    
+    private int findJpegSegment(byte[] a, int dataLen, int segmentType) {
+        int p = 2;
+        while (p <= dataLen - 6) {
+            if ((a[p] & 0xff) != 0xff) {
+                System.out.println("Unexpected JPEG data structure (marker expected).");
+                break;
+            }
+            int markerCode = a[p + 1] & 0xff;
+            if (markerCode == segmentType) {
+                return p;
+            }
+            if (markerCode >= 0xD0 && markerCode <= 0xDA) {       // stop when scan data begins
+                break;
+            }
+            int len = ((a[p + 2] & 0xff) << 8) + (a[p + 3] & 0xff);
+            p += len + 2;
+        }
+        return -1;
+    }
+    
+    
+    
+    // see 10918-1:1994, K.3.3.1 Specification of typical tables for DC difference coding
+    private static byte[] mjpgHuffmanTable = {
+            (byte) 0xff, (byte) 0xc4, (byte) 0x01, (byte) 0xa2, (byte) 0x00, (byte) 0x00, (byte) 0x01, (byte) 0x05, (byte) 0x01, (byte) 0x01,
+            (byte) 0x01, (byte) 0x01, (byte) 0x01, (byte) 0x01, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+            (byte) 0x00, (byte) 0x00, (byte) 0x01, (byte) 0x02, (byte) 0x03, (byte) 0x04, (byte) 0x05, (byte) 0x06, (byte) 0x07, (byte) 0x08,
+            (byte) 0x09, (byte) 0x0a, (byte) 0x0b, (byte) 0x10, (byte) 0x00, (byte) 0x02, (byte) 0x01, (byte) 0x03, (byte) 0x03, (byte) 0x02,
+            (byte) 0x04, (byte) 0x03, (byte) 0x05, (byte) 0x05, (byte) 0x04, (byte) 0x04, (byte) 0x00, (byte) 0x00, (byte) 0x01, (byte) 0x7d,
+            (byte) 0x01, (byte) 0x02, (byte) 0x03, (byte) 0x00, (byte) 0x04, (byte) 0x11, (byte) 0x05, (byte) 0x12, (byte) 0x21, (byte) 0x31,
+            (byte) 0x41, (byte) 0x06, (byte) 0x13, (byte) 0x51, (byte) 0x61, (byte) 0x07, (byte) 0x22, (byte) 0x71, (byte) 0x14, (byte) 0x32,
+            (byte) 0x81, (byte) 0x91, (byte) 0xa1, (byte) 0x08, (byte) 0x23, (byte) 0x42, (byte) 0xb1, (byte) 0xc1, (byte) 0x15, (byte) 0x52,
+            (byte) 0xd1, (byte) 0xf0, (byte) 0x24, (byte) 0x33, (byte) 0x62, (byte) 0x72, (byte) 0x82, (byte) 0x09, (byte) 0x0a, (byte) 0x16,
+            (byte) 0x17, (byte) 0x18, (byte) 0x19, (byte) 0x1a, (byte) 0x25, (byte) 0x26, (byte) 0x27, (byte) 0x28, (byte) 0x29, (byte) 0x2a,
+            (byte) 0x34, (byte) 0x35, (byte) 0x36, (byte) 0x37, (byte) 0x38, (byte) 0x39, (byte) 0x3a, (byte) 0x43, (byte) 0x44, (byte) 0x45,
+            (byte) 0x46, (byte) 0x47, (byte) 0x48, (byte) 0x49, (byte) 0x4a, (byte) 0x53, (byte) 0x54, (byte) 0x55, (byte) 0x56, (byte) 0x57,
+            (byte) 0x58, (byte) 0x59, (byte) 0x5a, (byte) 0x63, (byte) 0x64, (byte) 0x65, (byte) 0x66, (byte) 0x67, (byte) 0x68, (byte) 0x69,
+            (byte) 0x6a, (byte) 0x73, (byte) 0x74, (byte) 0x75, (byte) 0x76, (byte) 0x77, (byte) 0x78, (byte) 0x79, (byte) 0x7a, (byte) 0x83,
+            (byte) 0x84, (byte) 0x85, (byte) 0x86, (byte) 0x87, (byte) 0x88, (byte) 0x89, (byte) 0x8a, (byte) 0x92, (byte) 0x93, (byte) 0x94,
+            (byte) 0x95, (byte) 0x96, (byte) 0x97, (byte) 0x98, (byte) 0x99, (byte) 0x9a, (byte) 0xa2, (byte) 0xa3, (byte) 0xa4, (byte) 0xa5,
+            (byte) 0xa6, (byte) 0xa7, (byte) 0xa8, (byte) 0xa9, (byte) 0xaa, (byte) 0xb2, (byte) 0xb3, (byte) 0xb4, (byte) 0xb5, (byte) 0xb6,
+            (byte) 0xb7, (byte) 0xb8, (byte) 0xb9, (byte) 0xba, (byte) 0xc2, (byte) 0xc3, (byte) 0xc4, (byte) 0xc5, (byte) 0xc6, (byte) 0xc7,
+            (byte) 0xc8, (byte) 0xc9, (byte) 0xca, (byte) 0xd2, (byte) 0xd3, (byte) 0xd4, (byte) 0xd5, (byte) 0xd6, (byte) 0xd7, (byte) 0xd8,
+            (byte) 0xd9, (byte) 0xda, (byte) 0xe1, (byte) 0xe2, (byte) 0xe3, (byte) 0xe4, (byte) 0xe5, (byte) 0xe6, (byte) 0xe7, (byte) 0xe8,
+            (byte) 0xe9, (byte) 0xea, (byte) 0xf1, (byte) 0xf2, (byte) 0xf3, (byte) 0xf4, (byte) 0xf5, (byte) 0xf6, (byte) 0xf7, (byte) 0xf8,
+            (byte) 0xf9, (byte) 0xfa, (byte) 0x01, (byte) 0x00, (byte) 0x03, (byte) 0x01, (byte) 0x01, (byte) 0x01, (byte) 0x01, (byte) 0x01,
+            (byte) 0x01, (byte) 0x01, (byte) 0x01, (byte) 0x01, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+            (byte) 0x01, (byte) 0x02, (byte) 0x03, (byte) 0x04, (byte) 0x05, (byte) 0x06, (byte) 0x07, (byte) 0x08, (byte) 0x09, (byte) 0x0a,
+            (byte) 0x0b, (byte) 0x11, (byte) 0x00, (byte) 0x02, (byte) 0x01, (byte) 0x02, (byte) 0x04, (byte) 0x04, (byte) 0x03, (byte) 0x04,
+            (byte) 0x07, (byte) 0x05, (byte) 0x04, (byte) 0x04, (byte) 0x00, (byte) 0x01, (byte) 0x02, (byte) 0x77, (byte) 0x00, (byte) 0x01,
+            (byte) 0x02, (byte) 0x03, (byte) 0x11, (byte) 0x04, (byte) 0x05, (byte) 0x21, (byte) 0x31, (byte) 0x06, (byte) 0x12, (byte) 0x41,
+            (byte) 0x51, (byte) 0x07, (byte) 0x61, (byte) 0x71, (byte) 0x13, (byte) 0x22, (byte) 0x32, (byte) 0x81, (byte) 0x08, (byte) 0x14,
+            (byte) 0x42, (byte) 0x91, (byte) 0xa1, (byte) 0xb1, (byte) 0xc1, (byte) 0x09, (byte) 0x23, (byte) 0x33, (byte) 0x52, (byte) 0xf0,
+            (byte) 0x15, (byte) 0x62, (byte) 0x72, (byte) 0xd1, (byte) 0x0a, (byte) 0x16, (byte) 0x24, (byte) 0x34, (byte) 0xe1, (byte) 0x25,
+            (byte) 0xf1, (byte) 0x17, (byte) 0x18, (byte) 0x19, (byte) 0x1a, (byte) 0x26, (byte) 0x27, (byte) 0x28, (byte) 0x29, (byte) 0x2a,
+            (byte) 0x35, (byte) 0x36, (byte) 0x37, (byte) 0x38, (byte) 0x39, (byte) 0x3a, (byte) 0x43, (byte) 0x44, (byte) 0x45, (byte) 0x46,
+            (byte) 0x47, (byte) 0x48, (byte) 0x49, (byte) 0x4a, (byte) 0x53, (byte) 0x54, (byte) 0x55, (byte) 0x56, (byte) 0x57, (byte) 0x58,
+            (byte) 0x59, (byte) 0x5a, (byte) 0x63, (byte) 0x64, (byte) 0x65, (byte) 0x66, (byte) 0x67, (byte) 0x68, (byte) 0x69, (byte) 0x6a,
+            (byte) 0x73, (byte) 0x74, (byte) 0x75, (byte) 0x76, (byte) 0x77, (byte) 0x78, (byte) 0x79, (byte) 0x7a, (byte) 0x82, (byte) 0x83,
+            (byte) 0x84, (byte) 0x85, (byte) 0x86, (byte) 0x87, (byte) 0x88, (byte) 0x89, (byte) 0x8a, (byte) 0x92, (byte) 0x93, (byte) 0x94,
+            (byte) 0x95, (byte) 0x96, (byte) 0x97, (byte) 0x98, (byte) 0x99, (byte) 0x9a, (byte) 0xa2, (byte) 0xa3, (byte) 0xa4, (byte) 0xa5,
+            (byte) 0xa6, (byte) 0xa7, (byte) 0xa8, (byte) 0xa9, (byte) 0xaa, (byte) 0xb2, (byte) 0xb3, (byte) 0xb4, (byte) 0xb5, (byte) 0xb6,
+            (byte) 0xb7, (byte) 0xb8, (byte) 0xb9, (byte) 0xba, (byte) 0xc2, (byte) 0xc3, (byte) 0xc4, (byte) 0xc5, (byte) 0xc6, (byte) 0xc7,
+            (byte) 0xc8, (byte) 0xc9, (byte) 0xca, (byte) 0xd2, (byte) 0xd3, (byte) 0xd4, (byte) 0xd5, (byte) 0xd6, (byte) 0xd7, (byte) 0xd8,
+            (byte) 0xd9, (byte) 0xda, (byte) 0xe2, (byte) 0xe3, (byte) 0xe4, (byte) 0xe5, (byte) 0xe6, (byte) 0xe7, (byte) 0xe8, (byte) 0xe9,
+            (byte) 0xea, (byte) 0xf2, (byte) 0xf3, (byte) 0xf4, (byte) 0xf5, (byte) 0xf6, (byte) 0xf7, (byte) 0xf8, (byte) 0xf9, (byte) 0xfa};
+    
+    
+}
